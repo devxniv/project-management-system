05396d88ea9c85e2b525c27ea574ca656b867f64
Nivedita Bedarkar <niveditabedarkar@gmail.com>
2026-03-06 14:19:24 +0530

    feat(models): create project, task and note mongoose schemas

---
+++ src/models/project.models.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const projectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+//Create a Mongoose model called project using the structure defined in projectSchema.
+export const Project = mongoose.model("Project", projectSchema);
