a0b06fbdef63853491fb0d5c08974dc46a3d3f3d
Nivedita Bedarkar <niveditabedarkar@gmail.com>
2026-03-11 12:52:28 +0530

    feat: Role-Based Access Control middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
-
+import { ProjectMember } from "../models/projectmember.models.js";
 export const verifyJWT = asyncHandler(async (req, resizeBy, next) => {
   const token =
     req.cookies?.accessToken ||
@@ -26,3 +26,37 @@ export const verifyJWT = asyncHandler(async (req, resizeBy, next) => {
     throw new ApiError(401, "Invallid access token");
   }
 });
+
+//Role-Based Access Control middleware
+export const validateProjectPermission = (roles = []) => {
+  asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw new ApiError(400, "project id is missing");
+    }
+
+    //project member document
+    const project = await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user._id),
+    });
+
+    if (!project) {
+      throw new ApiError(400, "project not found");
+    }
+
+    const givenRole = project?.role;
+
+    //attach the "givenRole' to "req.user.role" so that subsequent functions know exactly what the user's permissions are.
+    req.user.role = givenRole;
+
+    if (!roles.includes(givenRole)) {
+      throw new ApiError(
+        403,
+        "you do not have permission to perform this action",
+      );
+    }
+    next();
+  });
+};
